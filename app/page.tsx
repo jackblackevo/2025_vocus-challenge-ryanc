e89d175a11e7dba4ff29ee1d14060b043c9900d4
ryan.c <jackblackevo@gmail.com>
2025-09-06 13:40:39 +0800

    feat: render random article page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { getArticles } from '../features/articles/apis';
+import ArticleList from '../features/articles/components/ArticleList';
 
 const AUTHOR_ID = '601aa114fd89780001d24d4d';
 
@@ -18,6 +19,21 @@ export async function generateMetadata(): Promise<Metadata> {
   };
 }
 
-export default function Home() {
-  return <div>Hello World!</div>;
+export default async function Home() {
+  const { count: articleCount } = await getArticles({
+    authorId: AUTHOR_ID,
+    page: 1,
+    num: 1,
+  });
+
+  const pageCount = Math.floor(articleCount / 4);
+  const pageNum = Math.ceil(Math.random() * pageCount);
+
+  const { articles } = await getArticles({
+    authorId: AUTHOR_ID,
+    page: pageNum,
+    num: 4,
+  });
+
+  return <ArticleList articles={articles} />;
 }
