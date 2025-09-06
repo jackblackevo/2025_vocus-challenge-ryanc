83b4c8f84e6f4d3b93ee81490f275a925961c48c
ryan.c <jackblackevo@gmail.com>
2025-09-06 21:41:46 +0800

    fix: ensure metadata and page fetch same articles

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,11 @@
+import { cache } from 'react';
 import type { Metadata } from 'next';
 import { getArticles } from '../features/articles/apis';
 import ArticleList from '../features/articles/components/ArticleList';
 
 const AUTHOR_ID = '601aa114fd89780001d24d4d';
 
-export async function generateMetadata(): Promise<Metadata> {
-  const response = await getArticles({ authorId: AUTHOR_ID, page: 1, num: 1 });
-  const article = response.articles.at(0);
-
-  return {
-    title: article?.title,
-    description: article?.abstract,
-    openGraph: {
-      title: article?.title,
-      description: article?.abstract,
-      images: article?.thumbnailUrl,
-    },
-  };
-}
-
-export default async function Home() {
+const getRandomFourArticles = cache(async () => {
   const { count: articleCount } = await getArticles({
     authorId: AUTHOR_ID,
     page: 1,
@@ -34,6 +20,27 @@ export default async function Home() {
     page: pageNum,
     num: 4,
   });
+
+  return articles;
+});
+
+export async function generateMetadata(): Promise<Metadata> {
+  const articles = await getRandomFourArticles();
+  const firstArticle = articles.at(0);
+
+  return {
+    title: firstArticle?.title,
+    description: firstArticle?.abstract,
+    openGraph: {
+      title: firstArticle?.title,
+      description: firstArticle?.abstract,
+      images: firstArticle?.thumbnailUrl,
+    },
+  };
+}
+
+export default async function Home() {
+  const articles = await getRandomFourArticles();
 
   return <ArticleList articles={articles} />;
 }
