c16105d9979a0289782a8f8acc3c2fcc0401a9dc
ryan.c <jackblackevo@gmail.com>
2025-09-06 10:00:03 +0800

    feat: set language to traditional chinese

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="zh-Hant">
       <body className={notoSansTC.className}>
         <StyledComponentsRegistry>
           <GlobalStyle />
