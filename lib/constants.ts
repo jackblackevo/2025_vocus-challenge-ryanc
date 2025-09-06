4d78212e51897c2f6fa31f258aaf396aa5b66c2c
ryan.c <jackblackevo@gmail.com>
2025-09-06 13:44:31 +0800

    fix: add remote image host for next app

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,7 @@
 export const API_ENDPOINT = 'https://api-staging.vocus.cc';
 
+export const IMG_ENDPOINT = 'https://images.vocus.cc';
+
 export const BREAKPOINTS = {
   md: 768,
 };
--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from 'next';
+import { IMG_ENDPOINT } from './lib/constants';
 
 const nextConfig: NextConfig = {
   /* config options here */
   compiler: {
     styledComponents: true,
+  },
+  images: {
+    remotePatterns: [new URL(`${IMG_ENDPOINT}/**`)],
   },
 };
 
